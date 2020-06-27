5f15aa042cdcf323aa408b69cada682c070f1674
C. Serrano <serrano.cjm@gmail.com>
2020-06-27 14:09:53 -0400

    Add constants and extensions

---
+++ app/src/main/java/com/caparepa/basemvvmboilerplate/utils/Constants.kt
@@ -0,0 +1,20 @@
+package com.caparepa.basemvvmboilerplate.utils
+
+//Preferences
+const val KEY_SAVED_AT = "key_saved_at"
+
+//Forecast DAO
+const val FUTURE_DAYS_FETCH = 16
+
+//Other
+const val MINIMUM_INTERVAL = 6
+const val WEATHER_DATA_ID = 0
+const val WEATHER_DESCRIPTION_ID = 0
+const val FUTURE_WEATHER_LOCATION_DATA_ID = 0
+
+//networking
+const val USE_DEVICE_LOCATION = "USE_DEVICE_LOCATION"
+const val CUSTOM_LOCATION = "CUSTOM_LOCATION"
+
+//unit
+const val UNIT_SYSTEM = "UNIT_SYSTEM"
